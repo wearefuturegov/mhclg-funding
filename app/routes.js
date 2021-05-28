708d1500fcc6e4489fa36d28f4bce9b475c28035
Chris Evans <evanschris@users.noreply.github.com>
2021-05-28 17:17:55 +0100

    in progress

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,10 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+
+router.get('funds', function (req, res) {
+    res.render('funds', { name: 'Foo' })
+})
+
+
 module.exports = router
